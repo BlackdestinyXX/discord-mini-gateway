23b705f580c3b3cca177cc9e93a3867a939c0871
BlackdestinyXX <blackdestinyx145@gmail.com>
2024-10-19 19:00:38 +0200

    Completato quarto e quinto passaggio: handler per le disconnessioni, resuming

--- src/Client.ts
+++ src/Client.ts
@@ -40,7 +40,7 @@ export default class Client {
 
     constructor({ shards, intents }: ClientOptions = {}) {
         if (shards) this.shards = shards;
-        if(intents) this.intents = intents;
+        if (intents) this.intents = intents;
     }
 
     private async getGatewayBot(): Promise<BotGatewayResponse> {
@@ -72,55 +72,135 @@ export default class Client {
         }, this.heartbeat_interval || 42000)
     }
 
+    private async openConnection(connectionUrl?: string) {
+        this.websocket = new WebSocket(connectionUrl || this.url + "?v=10&encoding=json");
+
+        this.websocket.on('error', console.error);
+
+        this.websocket.on('close', (event) => {
+            switch (event) {
+                case 4000:
+                    console.log("Unknown error")
+                    this.resume()
+                    break;
+                case 4001:
+                    console.log("Unknown opcode")
+                    this.resume()
+                    break;
+                case 4002:
+                    console.log("Decode error")
+                    this.resume()
+                    break;
+                case 4003:
+                    console.log("Not authenticated")
+                    this.resume()
+                    break;
+                case 4004:
+                    console.log("Authentication failed")
+                    break;
+                case 4005:
+                    console.log("Already authenticated")
+                    this.resume()
+                    break;
+                case 4007: 
+                    console.log("Invalid seq")
+                    this.resume()
+                    break;
+                case 4008:
+                    console.log("Rate limited")
+                    break;
+                case 4009:
+                    console.log("Session timeout")
+                    this.resume()
+                    break;
+                case 4010:
+                    console.log("Invalid shard")
+                    break;
+                case 4011:
+                    console.log("Sharding required")
+                    break;
+                case 4012:
+                    console.log("Invalid API version")
+                    break;
+                case 4013:
+                    console.log("Invalid intent(s)")
+                    break;
+                case 4014:
+                    console.log("Disallowed intent(s)")
+                    break;
+            }
+        })
+
+        this.websocket.on('open', () => {
+            console.log("Connection started")
+        });
+
+        this.websocket.on('message', (data: Payload) => {
+
+            let parsedData = JSON.parse(data.toString())
+
+            if (parsedData.s) this.last_sequence = parsedData.s;
+
+            switch (parsedData.op) {
+                case 1:
+                    this.beat()
+                    break;
+                case 10:
+                    this.hello(parsedData)
+                    this.startHeartbeating()
+                    break;
+                case 11:
+                    console.log("Heartbeat acknowledged")
+                    break;
+                case 0:
+                    if (parsedData.t == "READY") {
+                        console.log("Received dispatch event: client ready")
+                        this.resume_gateway_url = parsedData.resume_gateway_url
+                        this.session_id = parsedData.session_id
+                    }
+                    break;
+                case 9:
+                    console.log("Invalid session")
+                    if(parsedData.d == true) {
+                        this.resume()
+                    }
+                    break;
+                case 7:
+                    console.log("Received reconnect event")
+                    this.resume()
+                    break;
+            }
+
+        });
+    }
+
     async connect(token: string) {
 
         this.token = token;
 
-        this.getGatewayBot().then(data => {
+        this.getGatewayBot().then(() => {
 
-            const ws = new WebSocket(data.url + "?v=10&encoding=json");
-
-            this.websocket = ws;
-
-            ws.on('error', console.error);
-
-            ws.on('open', () => {
-                console.log("Connection started")
-            });
-
-            ws.on('message', (data: Payload) => {
-
-                let parsedData = JSON.parse(data.toString())
-
-                if(parsedData.s) this.last_sequence = parsedData.s;
-
-                switch(parsedData.op) {
-                    case 1:
-                        this.beat()
-                    break;
-                    case 10:
-                        this.hello(parsedData)
-                        this.startHeartbeating()
-                    break;
-                    case 11:
-                        console.log("Heartbeat acknowledged")
-                    break;
-                    case 0:
-                        if(parsedData.t == "READY") {
-                            console.log("Received dispatch event: client ready")
-                            this.resume_gateway_url = parsedData.resume_gateway_url
-                            this.session_id = parsedData.session_id
-                        }
-                    break;
-                    case 9:
-                        console.log("Invalid session")
-                    break;
-                }
-
-            });
+            this.openConnection();
 
         })
 
+    }
+
+    resume() {
+        this.openConnection(this.resume_gateway_url).then(() => {
+            this.websocket?.send(JSON.stringify({
+                op: 6,
+                d: {
+                    token: this.token,
+                    session_id: this.session_id,
+                    seq: this.last_sequence
+                }
+            }))
+        })
+    }
+
+    disconnect() {
+        this.websocket?.close(1000, "Disconnecting")
     }
 
     beat() {
@@ -147,7 +227,7 @@ export default class Client {
     }
 
     hello(payload: Payload) {
-        if(payload.d.heartbeat_interval) {
+        if (payload.d.heartbeat_interval) {
             this.heartbeat_interval = payload.d.heartbeat_interval
             const jitter = Math.random()
 
